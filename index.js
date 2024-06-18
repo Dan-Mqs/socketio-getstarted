30e184d31d0a1d5497acea02f2bdc0c0746237ad
danmqs <daniel.marques@sudema.pb.gov.br>
2024-06-17 22:28:04 -0300

    add socket.io and serve html

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+const __dirname = import.meta.dirname;
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello</h1>");
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on("connection", (socket) => {
+    console.log("user connected");
 })
 
 server.listen(3000, ()=> {
     console.log("server listening on :3000");
-})
+})  
